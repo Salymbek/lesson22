87e90f95015d98c68a4ed2ed979e59fb2159d3bf
Salymbek <calumbekcaatbekov@gmail.com>
2022-12-01 15:59:41 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Random random = new Random();
+
+
+
+        ArrayList<Integer>numbers = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+        numbers.add(random.nextInt(1,100));
+
+        }
+
+        ArrayList<Integer>jupSan = new ArrayList<>();
+        ArrayList<Integer>takSan = new ArrayList<>();
+
+
+        for (Integer san:numbers) {
+            if (san % 2 ==0){
+                jupSan.add(san);
+            }else {
+                takSan.add(san);
+            }
+        }
+        System.out.println("JUP SANDAR");
+        jupSan.forEach(System.out::println);
+        System.out.println("TAK SANDAR");
+        takSan.forEach(System.out::println);
     }
 }
